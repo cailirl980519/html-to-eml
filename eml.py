0f4ccec6ce3933c5ba8661df4e85919895f93770
cailirl980519 <cailirl980519@gmail.com>
2019-12-19 15:47:38 +0800

    fix image bug and add send function

--- eml.py
+++ eml.py
@@ -1,58 +1,118 @@
 from os import listdir
 from os.path import isfile, isdir, join
+import os
 from email import generator
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from email.mime.image import MIMEImage
+from email.utils import make_msgid
+from bs4 import BeautifulSoup
 import codecs
-import re
+import base64
+import smtplib
 
 class Gen_Emails(object):
-    def __init__(self):
-        pass
+	def __init__(self):
+		self.send = 1
+		self.path = os.path.dirname(__file__)
+		self.output = os.path.dirname(__file__)
+		self.recepiant = '123@xyz.com'
+		self.option()
+		# self.FindFiles()
 
-    def FindFiles(self):
-        path = input('Enter folder path:')
-        outputpath = input('Enter output path:')
-        files = listdir(path)
-        for f in files:
-            # 產生檔案的絕對路徑
-            fullpath = join(path, f)
-            # 判斷 fullpath 是檔案還是目錄
-            if isfile(fullpath) and 'html' in f:
-                if not("._" in f):
-                    self.EmailGen(fullpath, f, outputpath)
+	def option(self):
+		print (self.path)
+		print('1.Save as .eml\n')
+		print('2.Send email\n')
+		print('3.Above both\n')
+		self.send = input('Enter option:') 
+		if(int(self.send)>3):
+			print('Error')
+		else:
+			self.FindFiles()
 
-    def EmailGen(self, path, filename, output):
-        sender = ''
-        recepiant = ''
-        subject = filename.replace('.html','')
 
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['To'] = recepiant
 
-        html = codecs.open(path, 'r', 'utf-8')
-        # html = """\
-        # <html>
-        #     <head></head>
-        #     <body>
-        #         <p> hello world </p>
-        #     </body>
-        # </html>
-        # """
-        part = MIMEText(html.read(), 'html')
+	def FindFiles(self):
+		self.path = input('Enter folder path:')
+		if(int(self.send) == 1 or int(self.send) == 3):
+			self.output = input('Enter output path:')
+		if(int(self.send) == 2 or int(self.send) == 3):
+			self.recepiant = input('Enter Email address:')
+		files = listdir(self.path)
+		for f in files:
+			# 產生檔案的絕對路徑
+			fullpath = join(self.path, f)
+			# 判斷 fullpath 是檔案還是目錄
+			if isfile(fullpath) and 'html' in f:
+				if not("._" in f):
+					html = codecs.open(fullpath, 'r', 'utf-8')
+					self.EmailGen(f, html)  	
 
-        msg.attach(part)
+	def EmailGen(self, filename, html):
+		data = []
+		cid_list = []
+		
+		soup = BeautifulSoup(html, 'html.parser')
+		img_tag = soup.find_all('img')
+		for tag in img_tag:
+			try:
+				src = tag['src']
+			except:
+				pass
+			if 'data:image' in src:
+				data.append(src)
+				cid = make_msgid(domain='yahoo.com.tw')[1:-1]
+				cid_list.append(cid)
+				tag['src'] = "cid:{}".format(cid)
 
-        self.SaveToFile(msg, subject, output)
+		html = soup.prettify()
 
-    def SaveToFile(self,msg,filename,output):
-        file = filename + '.eml'
-        with open(join(output, file), 'w') as outfile:
-            gen = generator.Generator(outfile)
-            gen.flatten(msg)
+		sender = ''
+		recepiant = ''
+		subject = filename.replace('.html','')
+
+		msg = MIMEMultipart('mixed')
+		msgImg = MIMEMultipart('related')		
+
+		part = MIMEText(html, 'html')
+
+		msgImg.attach(part)
+
+		for i, img in enumerate(data, start=0):
+			fp = base64.b64decode(img.split(',')[1])
+			msgImage = MIMEImage(fp, _subtype='png')
+			msgImage.add_header('Content-ID', cid_list[i])
+			msgImg.attach(msgImage)
+
+		msg.attach(msgImg)
+
+		if(int(self.send) == 1 or int(self.send) == 3):
+			msg['Subject'] = subject
+			msg['From'] = sender
+			msg['To'] = recepiant
+			self.SaveToFile(msg, subject)
+		if(int(self.send) == 2 or int(self.send) == 3):
+			msg['Subject'] = subject
+			self.Send(msg)
+
+	def SaveToFile(self, msg, filename):
+		file = filename + '.eml'
+		with open(join(self.output, file), 'w') as outfile:
+			gen = generator.Generator(outfile)
+			gen.flatten(msg)
+
+	def Send(self, msg):
+		try:
+			msg['From'] = 'd1997721@yahoo.com.tw'
+			msg['To'] = self.recepiant
+			s = smtplib.SMTP('smtp.mail.yahoo.com:587')
+			s.starttls()
+			s.login('d1997721@yahoo.com.tw',"D_a_n_i_e_l_19")
+			s.sendmail('d1997721@yahoo.com.tw', self.recepiant, msg.as_string())
+			s.quit()
+			print('success')
+		except smtplib.SMTPException:
+			print('Error')
 
 x = Gen_Emails()
-x.FindFiles()
